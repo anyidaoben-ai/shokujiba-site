ec4dd7d66987afbd2a8e7ec982f215e1ebf0dbf6
asato <asato@klerkwek.local>
2026-07-02 00:35:58 +0900

    j

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,10 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
     },
     {
+      url: `${baseUrl}/shokujiba`,
+      lastModified: new Date(),
+    },
+    {
       url: `${baseUrl}/Dispense`,
       lastModified: new Date(),
     },
